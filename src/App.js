0079e596906d6471195e5f5299e9bbd81bd7e077
zhangyongwang <765298337@qq.com>
2020-05-19 10:55:35 +0800

    init

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div className='wrap'>
+        <div className='section_side'>
+          12
+        </div>
+        <div className='section_middle'>
+          1
+        </div>
+        <div className='section_side'>
+          12
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import './asset/css/Index.css'
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
